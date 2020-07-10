a341a2da0d81b8369263e1e245e41620f1a78f99
s-mobe <omar.m.abdelfattah@gmail.com>
2020-07-10 07:25:00 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.common.exceptions import NoSuchElementException
+
+browser = webdriver.Firefox()
+
+browser.maximize_window()
+
+browser.get('http://www.facebook.com')
+username = "omar.magdi@live.com"
+password = "Magno+123"
+
+
+
+user = browser.find_element_by_id('email')
+passw = browser.find_element_by_id('pass')
+try:
+    logbtn = browser.find_element_by_id('loginbutton')
+except NoSuchElementException:
+    logbtn = browser.find_element_by_id("u_0_d")
+
+user.send_keys(username)
+passw.send_keys(password)
+time.sleep(3)
+
+logbtn.click()
+time.sleep(4)
+
+browser.get("https://www.facebook.com/events/birthdays/")
+time.sleep(5)
+bdlist = browser.find_elements_by_css_selector("._1mf")  # class="_1mf _1mj
+
+if len(bdlist) == 0 :
+    print("couldnt fetch the class name properly, FB changed the class name")
+
+
+
+for bd in bdlist:
+    action = webdriver.ActionChains(browser)
+    action.click(bd)
+    action.send_keys("Happy Birthday, wish you an amazing year")
+    action.send_keys(Keys.RETURN)
+    action.perform()
+    bd.location_once_scrolled_into_view
+
+    time.sleep(5)
+    
